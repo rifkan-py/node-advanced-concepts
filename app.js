b416d9f8cad9a83e4117a3929ff661d8e7b7d318
rifkan <mohamedrifkan00127@gmail.com>
2023-04-09 21:05:57 +0530

    added second middleware and tested the order of the execution

--- app.js
+++ app.js
@@ -7,6 +7,17 @@ function middleware(req, res, next) {
   next()
 }
 
+function middleware2(req, res, next) {
+  console.log("This is running from middleware 2")
+  next()
+}
+
+app.use(middleware2)
+
+// 1st middleware2 will execute
+// and then actual middleware will execute
+// after that controller middleware will execute
+
 app.get("/", middleware, (req, res) => {
   const {user} = req
   res.send(`<h1>Hello, World! ${user} </h1>`)

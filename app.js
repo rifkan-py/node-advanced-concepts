bc2cd4e95e7d714a2469e9cf268e7dfe006d1b5d
rifkan <mohamedrifkan00127@gmail.com>
2023-04-12 07:56:44 +0530

    learn somethig about passport and passport local strategy

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const express = require('express')
 const session = require('express-session')
+const bcrypt = require('bcrypt')
 
 const app = express()
 
+const users = []
+
 app.set("view-engine", "ejs")
+app.use(express.urlencoded({extended: false}))
 app.use(session({
   secret: "some-secret",
   resave: false,
@@ -32,12 +36,54 @@ app.use(middleware2)
 
 app.get("/", middleware, (req, res) => {
   const {user} = req
-  res.render("index.ejs")
+  res.render("index.ejs", {name: "Rifkan"})
+})
+
+app.get("/login", middleware, (req, res) => {
+  const {user} = req
+  res.render("login.ejs", {name: "Rifkan"})
+})
+
+app.post("/login", (req, res) => {
+
+})
+
+app.post("/register", async (req, res) => {
+  try {
+
+    if(req.body.email && req.body.password && req.body.confirm_password) {
+      if(req.body.password !== req.body.confirm_password) {
+        throw new Error("Passwords do not match")
+      }
+  
+      const hashedPassword = await bcrypt.hash(req.body.password, 10)
+      users.push({
+        id: Date.now().toString(),
+        email: req.body.email,
+        password: hashedPassword
+      })
+      res.redirect("/login")
+    } else {
+      throw new Error("all the fields are required")
+    }
+  } catch(error) {
+    res.redirect("/register")
+  }
+
+  console.log(users)
+})
+
+app.get("/register", middleware, (req, res) => {
+  const {user} = req
+  res.render("register.ejs", {name: "Rifkan"})
 })
 
 function errorHandler(error, req, res, next) {
+  console.log(error)
   if(error) return res.send("Please try again.")
 }
+
+app.use(errorHandler)
 
 
 app.listen(8000, () => console.log("server is listening on port 8000"))
